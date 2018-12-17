0309b51829483efae8f6a258e188b1b0da10f18b
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-12-17 23:39:13 +0600

    fetch next chunk of products

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,26 +2,69 @@ import React, { Component } from 'react'
 
 import classes from './Products.css'
 
+const scroll = that => {
+  // Scroll
+  window.onscroll = function() {
+    var d = document.documentElement
+    var offset = d.scrollTop + window.innerHeight
+    var height = d.offsetHeight
+
+    // console.log('offset = ' + Math.ceil(offset))
+    // console.log('height = ' + height)
+
+    if (Math.ceil(offset) === height) {
+      console.log('At the bottom')
+      // console.log(that.state)
+      that.fetchDataNextPage()
+    }
+  }
+}
+
 class Products extends Component {
   constructor() {
     super()
     this.state = {
-      products: []
+      products: [],
+      page: 1,
+      limit: 15
     }
+    this.fetchDataNextPage.bind(this)
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/products')
+    fetch(
+      `http://localhost:3000/api/products?_page=${this.state.page}&_limit=${
+        this.state.limit
+      }`
+    )
       .then(res => res.json())
       .then(json => {
         console.log(json)
         this.setState({ products: json })
       })
       .catch(err => console.log(err))
+
+    scroll(this)
+  }
+
+  fetchDataNextPage() {
+    fetch(
+      `http://localhost:3000/api/products?_page=${this.state.page + 1}&_limit=${
+        this.state.limit
+      }`
+    )
+      .then(res => res.json())
+      .then(json => {
+        const { products } = this.state
+        const newProducts = [...products, ...json]
+        this.setState({ products: newProducts, page: this.state.page + 1 })
+      })
+      .catch(err => console.log(err))
   }
 
   render() {
     const { products } = this.state
+    console.log(products)
     const productList = products.map(product => (
       <div className={classes.box} key={product.id}>
         {product.face}
