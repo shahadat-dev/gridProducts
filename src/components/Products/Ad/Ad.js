cc4f2880a59af5eb7986103e0e7c576467671177
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-12-20 22:55:11 +0600

    fix ad class | loading position

--- src/components/Products/Ad/Ad.js
+++ src/components/Products/Ad/Ad.js
@@ -5,7 +5,7 @@ import classes from './Ad.css'
 export default function Ad({ ad }) {
   return (
     <div className={classes.Box} key={name}>
-      <img class="ad" src={`http://localhost:3000/ads/?r=${ad.id}`} />
+      <img src={`http://localhost:3000/ads/?r=${ad.id}`} />
     </div>
   )
 }
